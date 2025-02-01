146e58b65a949f8907428b69b128e6c1e5a8b8a8
yeon <p-yo00@naver.com>
2025-02-02 06:39:16 +0900

    refactor: map으로 dto 변환
    - for문 대신 map을 사용해 dto를 변환하여 간결하게 한다.
    - 사진 파일 경로를 불러올 때 도메인을 concat

--- src/main/kotlin/com/yeoff/jpopkaraokeserver/service/SongService.kt
+++ src/main/kotlin/com/yeoff/jpopkaraokeserver/service/SongService.kt
@@ -5,60 +5,66 @@ import com.yeoff.jpopkaraokeserver.domain.constant.JpopException
 import com.yeoff.jpopkaraokeserver.domain.constant.SuccessCode
 import com.yeoff.jpopkaraokeserver.domain.dto.SongDetailRespDto
 import com.yeoff.jpopkaraokeserver.domain.dto.SongListRespDto
-import com.yeoff.jpopkaraokeserver.domain.dto.SuccessRespDto
-import com.yeoff.jpopkaraokeserver.domain.entity.LyricsEntity
+import com.yeoff.jpopkaraokeserver.domain.dto.common.SuccessRespDto
 import com.yeoff.jpopkaraokeserver.domain.entity.SongEntity
 import com.yeoff.jpopkaraokeserver.repository.LyricsRepository
 import com.yeoff.jpopkaraokeserver.repository.SongRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import kotlin.jvm.optionals.getOrNull
 
 @Service
 class SongService(
+    @Value("\${IMAGE_SERVER}")
+    private val imageServerDomain: String,
     private val songRepository: SongRepository,
     private val lyricsRepository: LyricsRepository
 ) {
     fun getJpopChart100(): SuccessRespDto<List<SongListRespDto>> {
-        val list: ArrayList<SongListRespDto> = ArrayList()
-
-        for (songEntity: SongEntity in songRepository.findAll()) {
-            list.add(SongListRespDto(
-                id = songEntity.id,
-                title = songEntity.title,
-                singer = songEntity.singer?.name,
-                originalTitle = songEntity.originalTitle,
-                albumImg = songEntity.albumImg,
-                ky = songEntity.ky,
-                tj = songEntity.tj
-            ))
-        }
-
-        return SuccessRespDto(SuccessCode.OK, list)
+        return SuccessRespDto(
+            successCode = SuccessCode.OK,
+            data = songRepository.findAll()
+                .map {
+                    SongListRespDto(
+                        id = it.id,
+                        title = it.title,
+                        singer = it.singer?.name,
+                        originalTitle = it.originalTitle,
+                        albumImg = imageServerDomain + it.albumImg,
+                        ky = it.ky,
+                        tj = it.tj
+                    )
+                }
+        )
     }
 
     fun getSongDetail(songId: Long): SuccessRespDto<SongDetailRespDto> {
         val songEntity: SongEntity? = songRepository.findById(songId).getOrNull()
         checkNotNull(songEntity) { throw JpopException(FailCode.SONG_NOT_FOUND) }
 
-        val lyricsEntities: List<LyricsEntity> = lyricsRepository.findBySong_IdOrderBySequence(songId)
-        val lyricsList: ArrayList<SongDetailRespDto.LyricsUnit> = ArrayList()
-        for (lyricsEntity: LyricsEntity in lyricsEntities) {
-            lyricsList.add(
-                SongDetailRespDto.LyricsUnit(
-                    kor = lyricsEntity.kor, jpn = lyricsEntity.jpn, rom = lyricsEntity.rom)
-            )
-        }
+        val lyricsList: List<SongDetailRespDto.LyricsUnit> =
+            lyricsRepository.findBySong_IdOrderBySequence(songId)
+                .map {
+                    SongDetailRespDto.LyricsUnit(
+                        kor = it.kor,
+                        jpn = it.jpn,
+                        rom = it.rom
+                    )
+                }
 
-        return SuccessRespDto(SuccessCode.OK, SongDetailRespDto(
-            id = songEntity.id,
-            title = songEntity.title,
-            singer = songEntity.singer?.name,
-            originalTitle = songEntity.originalTitle,
-            albumImg = songEntity.albumImg,
-            youtubeUrl = songEntity.youtubeUrl,
-            ky = songEntity.ky,
-            tj = songEntity.tj,
-            lyrics = lyricsList
-        ))
+        return SuccessRespDto(
+            successCode = SuccessCode.OK,
+            data = SongDetailRespDto(
+                id = songEntity.id,
+                title = songEntity.title,
+                singer = songEntity.singer?.name,
+                originalTitle = songEntity.originalTitle,
+                albumImg = imageServerDomain + songEntity.albumImg,
+                youtubeUrl = songEntity.youtubeUrl,
+                ky = songEntity.ky,
+                tj = songEntity.tj,
+                lyrics = lyricsList
+            )
+        )
     }
 }
