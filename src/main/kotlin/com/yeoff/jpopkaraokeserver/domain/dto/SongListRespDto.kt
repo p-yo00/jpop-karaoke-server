43ed96a3da166da73f602edf329547828070a920
yeon <p-yo00@naver.com>
2026-04-05 04:13:18 +0900

    feat: 응답 youtube url 추가

--- src/main/kotlin/com/yeoff/jpopkaraokeserver/domain/dto/SongListRespDto.kt
+++ src/main/kotlin/com/yeoff/jpopkaraokeserver/domain/dto/SongListRespDto.kt
@@ -1,7 +1,6 @@
 package com.yeoff.jpopkaraokeserver.domain.dto
 
 import com.yeoff.jpopkaraokeserver.domain.entity.SongEntity
-import com.yeoff.jpopkaraokeserver.domain.entity.SongTop100Entity
 import com.yeoff.jpopkaraokeserver.util.FileUtil
 
 class SongListRespDto(
@@ -11,7 +10,8 @@ class SongListRespDto(
     var singer: String?,
     var albumImg: String?,
     var ky: Int?,
-    var tj: Int?
+    var tj: Int?,
+    var youtubeUrl: String?,
 ) {
 
     companion object {
@@ -23,18 +23,8 @@ class SongListRespDto(
                 originalTitle = songEntity.originalTitle,
                 albumImg =  FileUtil.getImageUrl(songEntity.albumImg),
                 ky = songEntity.ky,
-                tj = songEntity.tj
-            )
-        }
-        fun from(songEntity: SongTop100Entity): SongListRespDto {
-            return SongListRespDto(
-                id = songEntity.id,
-                title = songEntity.title,
-                singer = songEntity.singer?.name,
-                originalTitle = songEntity.originalTitle,
-                albumImg =  FileUtil.getImageUrl(songEntity.albumImg),
-                ky = songEntity.ky,
-                tj = songEntity.tj
+                tj = songEntity.tj,
+                youtubeUrl = songEntity.youtubeUrl
             )
         }
     }
